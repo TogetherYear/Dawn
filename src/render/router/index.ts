bd12456b55c5601097b67c5b1ad1b6aa08b86739
Together <togetheryear@gmail.com>
2023-01-01 15:42:36 +0800

    Delete

--- src/render/router/index.ts
+++ src/render/router/index.ts
@@ -8,10 +8,7 @@ import DawnVue from '@render/views/Dawn/Dawn.vue'
 /**
  * 2
  */
-import HomeVue from '@render/views/Dawn/Children/Home/Home.vue'
-import MessageVue from '@render/views/Dawn/Children/Message/Message.vue'
-import HelpVue from '@render/views/Dawn/Children/Help/Help.vue'
-import SettingVue from '@render/views/Dawn/Children/Setting/Setting.vue'
+
 
 const routes: Array<RouteRecordRaw> = [
     {
